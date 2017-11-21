ba3f48cd54af93cf2712bf1ff46064aa7f491834
max <maxim.kuzn@gmail.com>
2017-11-21 23:18:15 +0300

    fixed

--- App/Config.php
+++ App/Config.php
@@ -14,7 +14,7 @@ class Config
 
     protected function __construct()
     {
-        include_once __DIR__ . '/configs/params.php';
+        $this->data = include_once __DIR__ . '/configs/params.php';
     }
     public static function getInstance()
     {
