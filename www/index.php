d81184256d8f375355992119f67bd4fd6baf7921
max <maxim.kuzn@gmail.com>
2017-12-27 01:37:39 +0300

    fixed

--- www/index.php
+++ www/index.php
@@ -4,12 +4,12 @@ $url = $_SERVER['REQUEST_URI'];
 $urlParts = explode('/', $url);
 $controller = empty($urlParts[1]) ? 'Index' : $urlParts[1];
 $actionType = empty($urlParts[2]) ? 'Default': $urlParts[2];
-$adminAccess = !empty($urlParts[3]) ? $urlParts[3] : null;
 
 try {
     $class ='\\App\\Controllers\\'.$controller;
     $ctrl = new $class;
     $ctrl->action($actionType);
+
 } catch (\App\Exceptions\ItemNotFoundException $error) {
     $error->pageNotFound();
     die;
