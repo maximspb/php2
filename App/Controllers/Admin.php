716c9bbb73aa30209758b90cc1cab108046af55b
max <maxim.kuzn@gmail.com>
2017-12-27 01:30:53 +0300

    fixed

--- App/Controllers/Admin.php
+++ App/Controllers/Admin.php
@@ -1,91 +1,60 @@
 <?php
 namespace App\Controllers;
-
-
 use App\Exceptions\DbRequestException;
 use App\Exceptions\ItemNotFoundException;
 use App\Models\Article;
 use App\Models\Author;
 use App\Exceptions\MultiException;
-
 class Admin extends Controller
 {
 
+    protected $article;
 
     protected function access()
     {
-        /**
-         * блокировка доступа к админ-панели. В рамках ДЗ выполнено
-         * через добавление "секретного" слова в URL,
-         * при наличии которого есть доступ, без него - нет.
-         */
-        $url = explode('/', $_SERVER['REQUEST_URI']);
-        if (in_array('Secret', $url)) {
-            return true;
-        } else {
-            return false;
-        }
+        return true;
     }
-
-    protected function actionSecret()
+    public function __construct()
     {
-        /**
-         * экшен для доступа к главной странице админ-панели
-         * при блокировке с паролем в URL
-         */
-        return $this->actionDefault();
+        parent::__construct();
+
+        if (!empty($_GET['id'])) :
+            $this->article = \App\Models\Article::findById($_GET['id']);
+        else :
+            $this->article = new Article();
+        endif;
     }
 
     protected function actionDefault()
     {
-
         $this->view->news = Article::getAll();
         $this->view->display(__DIR__ . '/../../admin/templates/index.php');
     }
 
-    protected function actionEdit()
+
+    protected function actionForm()
     {
-        $this->view->errors = [];
-        try {
-            $this->view->article = \App\Models\Article::findById($_GET['id']);
-            $this->view->authors = Author::getAll();
-            if (isset($_POST['save'])) :
-                $this->view->article->fill($_POST);
-                $this->view->article->save();
-                header('Location:/Admin/');
-                exit();
-            endif;
-        } catch (ItemNotFoundException $e) {
-            echo $e->pageNotFound();
-            exit();
-        } catch (MultiException $e) {
-            $this->view->errors = $e->getAlldata();
-        }
+        $this->view->article = $this->article;
+        $this->view->authors = Author::getAll();
         $this->view->display(__DIR__ . '/../../admin/templates/edit.php');
     }
 
-    protected function actionInsert()
+
+    protected function actionSave()
     {
-        $this->view->errors = [];
-        $this->view->article = new Article();
-        $this->view->authors = Author::getAll();
 
         try {
-            $this->view->article->fill($_POST);
-            try {
-                $this->view->article->save();
-            } catch (DbRequestException $e) {
-                echo $e->getMessage();
-                exit;
-            }
-            if (isset($_POST['save'])) :
-                header('Location:/Admin/');
-                exit();
-            endif;
+            $this->article->fill($_POST);
+            $this->article->save();
+            header('Location:/Admin/');
+            exit();
         } catch (MultiException $e) {
-            $this->view->errors = $e->getAlldata();
+                $this->view->article = $this->article;
+                $this->view->article->title =$_POST['title'];
+                $this->view->article->text =$_POST['text'];
+                $this->view->errors = $e->getAllData();
+                $this->actionForm();
         }
-        $this->view->display(__DIR__ . '/../../admin/templates/edit.php');
     }
 
     protected function actionDelete()
