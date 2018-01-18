3613c128a6dbbd7c3f21de4e976656c2dce019f0
max <maxim.kuzn@gmail.com>
2018-01-19 00:54:46 +0300

    404 template

---
+++ templates/404.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h2><?php echo $error->getMessage() ; ?></h2>
+<img src="/img/Norns.jpg" alt=""><br>
+<a href="/">Вернуться на главную</a><br>
+</body>
+</html>
