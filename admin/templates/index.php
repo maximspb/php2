0e10ef92e83ec510f433dda072b4d77e6f2e427e
maximspb <maxim.kuzn@gmail.com>
2018-01-31 16:47:46 +0300

    added AdminDataTable usage

--- admin/templates/index.php
+++ admin/templates/index.php
@@ -1,3 +1,14 @@
+<?php
+//массив $news передается в шаблон из контроллера
+$tbl = new \App\AdminDataTable($news, $functions = [
+    function ($model) {
+        return $model->id;
+    },
+    function ($model) {
+        return $model->title;
+    }
+]);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,32 +16,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Список новостей</title>
+    <title>Админ-панель</title>
 </head>
 <body>
 <h1>Список новостей</h1>
-<table>
-    <tr>
-        <th>№</th>
-        <th>Заголовок</th>
-    </tr>
-    <?php foreach ($news as $article) : ?>
-    <tr>
-        <td><?php echo $article->id ; ?></td>
-        <td><?php echo $article->title ; ?></td>
-        <td>
-            <a href="/Admin/Form/?id=<?php echo $article->id ; ?>">
-                Редактировать
-            </a>
-        </td>
-        <td>
-            <a href="/Admin/Delete/?id=<?php echo $article->id ; ?>">
-                Удалить
-            </a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table><br>
+<?php $tbl->render(__DIR__.'/admintable.php'); ?>
+<br>
 <a href="/Admin/Form">Создать новость</a>
+
 </body>
 </html>
